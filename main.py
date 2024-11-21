484439eb4e63b25e494f6d60a8e3d5a39e26fdeb
Elias Gläßer <elias.silas@web.de>
2024-11-21 23:05:46 +0100

    Stylized App

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import requests  # type: ignore
-from PyQt5.QtWidgets import (  # type: ignore
+from PyQt5.QtWidgets import ( # type: ignore
     QApplication,
     QWidget,
     QLabel,
@@ -16,6 +16,71 @@ from PyQt5.QtCore import Qt  # type: ignore
 class WeatherApp(QWidget):
     def __init__(self):
         super().__init__()
+        self.city_label = QLabel("Enter City Name: ", self)
+        self.city_input = QLineEdit(self)
+        self.get_weather_button = QPushButton("Get Weather", self)
+        self.temperature_label = QLabel("28°C", self)
+        self.emoji_label = QLabel("☀️", self)
+        self.description_label = QLabel("Sunny", self)
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle("WeatherApp")
+
+        # Virtual Layout Manager
+        vbox = QVBoxLayout()
+
+        # Add Widgets (from labels)
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+
+        self.setLayout(vbox)
+
+        #Align all Widgets except the button
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+
+        # Style-Sheet to Style the App
+        self.setStyleSheet("""
+            QLabel, QPushButton{
+                font-family: calibri;
+            }
+            QLabel#city_label{
+                font-size: 40px;
+                font-style: italic;
+            }
+            QLineEdit#city_input{
+                font-size: 40px;
+            }
+            QPushButton#get_weather_button{
+                font-size: 30px;
+                font-weight:bold;
+            }
+            QLabel#temperature_label{
+                font-size: 75px;
+            }
+            QLabel#emoji_label{
+                font-size: 100px;
+                font-family: Segoe UI Emoji;
+            }
+            QLabel#description_label{
+                font-size: 50px;
+            }
+        """)
 
 
 if __name__ == "__main__":
